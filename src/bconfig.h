3071e3af304226de9017952a950c0c4c52a5e860
Greg Millam <walker@deafcode.com>
2013-02-14 20:44:30 -0800

    Add bconfig - banana config globals.

---
+++ src/bconfig.h
@@ -0,0 +1,24 @@
+/* bconfig.h
+ */
+
+#ifndef _MY_BANANACONF_H_
+#define _MY_BANANACONF_H_
+
+#include "config.h"
+extern struct config *bconfig;
+#define bconf_get(name,def) conf_get(bconfig, name, def)
+#define bconf_int(name,def) conf_int(bconfig, name, def)
+#define bconf_is(name,def)  conf_is(bconfig, name, def)
+
+// HTTP config options
+#define LISTEN_HOST      bconf_get("listen_host", "0.0.0.0")
+#define LISTEN_PORT      bconf_int("listen_port", 4080)
+#define HTTP_SIGNATURE   bconf_get("http_signature", "Banana MUSH Client")
+#define HTTP_ROOT        bconf_get("file_root", "public")
+
+// Print warnings when long poll lasts longer than this.
+#define LONGPOLL_TIMEOUT bconf_int("longpoll_timeout", 120)
+
+#define _unused_ __attribute__ ((__unused__))
+
+#endif
